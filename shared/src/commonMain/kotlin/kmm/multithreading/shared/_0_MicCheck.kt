d359d98d13e151f1ab3fb293e915ac608fac4dd1
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2021-03-24 03:11:16 +0300

    Fix _0_MicCheck

--- shared/src/commonMain/kotlin/kmm/multithreading/shared/_0_MicCheck.kt
+++ shared/src/commonMain/kotlin/kmm/multithreading/shared/_0_MicCheck.kt
@@ -6,7 +6,9 @@ import kmm.multithreading.shared.util.CallWithCallback
 
 class _0_MicCheck : CallWithCallback {
 
+    private var counter = 0
+
     override fun call(callback: (Any) -> Unit) {
-        callback(0)
+        callback(counter++)
     }
 }
